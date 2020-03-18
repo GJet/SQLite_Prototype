5e7c934f479721b04b0fcec2f0b50629c304a536
Jason Drake <thedrake87@gmail.com>
2020-03-18 15:35:41 -0400

    Updated to add some additional user functions.

--- example_user_program.py
+++ example_user_program.py
@@ -1,4 +1,21 @@
 import sqlite3
+import os
+
+
+# define our clear function
+#TODO: Clear() function doesn't seem to be working
+def clear():
+    # for windows
+
+    if os.name == 'nt':
+        _ = os.system('cls')
+
+        # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = os.system('clear')
+
+
+#Start of program
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
 
@@ -6,15 +23,35 @@ while 1:
     user_command = input("Enter a command. (? to list commands)")
 
     if user_command == "?":
-        print("    Here are the options")
+        clear()
+        print("Here are the options:")
         print("    list_all")
         print("    lookup <variable_name>")
         print("    find <partial_name>")
         print("    exit")
+        clear()
 
-    if user_command == "list_all":
+    elif user_command == "list_all":
+        clear()
         for row in c.execute('SELECT * FROM variables ORDER BY var_name'):
             print(str(row[0]) + " = " + str(row[3]))
-    if user_command == "exit":
+
+    elif "find" in user_command:
+        clear()
+        split_command = user_command.split()
+        if len(split_command) > 1:
+            for row in c.execute('SELECT * FROM variables ORDER BY var_name'):
+                if split_command[1] in row[0]:
+                    print(row)
+        else:
+            print("find command syntax error.  Try again.")
+
+
+    elif user_command == "exit":
+        clear()
         conn.close()
         break
+
+    else:
+        clear()
+        print("We didn't understand your request. Try again")
